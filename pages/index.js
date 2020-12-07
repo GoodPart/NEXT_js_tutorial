22a859c97505a9ffd708e2faf770b830eb9dd7bb
GoodPart <40373934+GoodPart@users.noreply.github.com>
2020-12-07 11:01:04 +0900

    less 붙이기

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,27 @@
 import Link from 'next/link';
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import styles from '../styles/Home.module.css'
-import utilStyles from '../styles/utils.module.css'
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+// import styles from '../styles/Home.module.css';
+// import utilStyles from '../styles/utils.module.css';
+
+//less 예시
+import '../styles/styles.less';
 
 export default function Home() {
   return (
     <>
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-    </Layout>
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section>
+          <p className="example">[Your Self Introduction]</p>
+          <p>
+            (This is a sample website - you’ll be building a site like this on{' '}
+            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          </p>
+        </section>
+      </Layout>
     </>
-  )
+  );
 }
