ba0be68fbc56e5e57360352f211609886d0edb7a
GoodPart <pks940122@cttd.co.kr>
2020-12-22 00:14:35 +0900

    redux  붙이기
    1. redux는 적용됨.
    2. redux logger 사용
    3.  redux-wrapper 워링이 하나뜸

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,46 @@
-import '../styles/globals.less'
+import "../styles/globals.less";
 
-import '../styles/test.less'
-// import Header from '../components/Layout/Header/header'
-import Navbar from '../components/Layout/navigation/Navbar'
-import Head from 'next/head';
-import Footer from '../components/Layout/footer/footer'
+import "../styles/test.less";
+import Navbar from "../components/Layout/navigation/Navbar";
+import Head from "next/head";
+import Footer from "../components/Layout/footer/footer";
 
+//add redux
+import withRedux from "next-redux-wrapper";
+import { createStore, compose, applyMiddleware } from "redux";
+// import { Provider } from "react-redux";
+import reducer from "../reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
 
-import { faCheckSquare, faSpinner } from "@fortawesome/free-solid-svg-icons"
-import { faSquare } from "@fortawesome/free-regular-svg-icons"
+// import { faCheckSquare, faSpinner } from "@fortawesome/free-solid-svg-icons";
+// import { faSquare } from "@fortawesome/free-regular-svg-icons";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component }) {
   return (
+    // <Provider store={store}>
     <div className="wrapper">
       <Head>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
       </Head>
-      {/* <Header /> */}
       <Navbar />
       <div className="contents">
-      <Component {...pageProps} />
+        <Component />
       </div>
       <Footer />
     </div>
-  )
+    // </Provider>
+  );
 }
 
-export default MyApp
+const configureStore = (initialState, options) => {
+  const middlewares = [logger]; // 미들웨어들을 넣으면 된다.
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares));
+  const store = createStore(reducer, initialState, enhancer);
+  return store;
+};
+
+export default withRedux(configureStore)(MyApp);
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,51 @@
-import Link from 'next/link';
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { countPlusAction, countMinusAction } from "../reducers/count";
+
+import Link from "next/link";
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
 // import styles from '../styles/Home.module.css'
 
-import utilStyles from '../styles/utils.module.less'
+import utilStyles from "../styles/utils.module.less";
 
+const Home = () => {
+  const dispatch = useDispatch(); // dispatch를 사용하기 쉽게 하는 hook
+  const count = useSelector((state) => state.count); // store의 state를 불러오는 hook   store의 state 중에서 count의 state를 불러온다.
 
+  const onClickPlus = useCallback(() => {
+    // useCallback은 최적화를 위한 hook이다 이 앱에선 굳이 사용 안 해도 되는데 습관이 들면 좋기에 사용.
+    dispatch(countPlusAction());
+  }, []);
 
+  const onClickMinus = useCallback(() => {
+    dispatch(countMinusAction());
+  }, []);
 
-export default function Home() {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-    </Layout>
-  )
-}
+    <div>
+      카운트 : {count}
+      <button onClick={onClickPlus}>+</button>
+      <button onClick={onClickMinus}>-</button>
+    </div>
+  );
+};
+
+export default Home;
+
+// export default function Home() {
+//   return (
+//     <Layout home>
+//       <Head>
+//         <title>{siteTitle}</title>
+//       </Head>
+//       <section className={utilStyles.headingMd}>
+//         <p>[Your Self Introduction]</p>
+//         <p>
+//           (This is a sample website - you’ll be building a site like this on{' '}
+//           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+//         </p>
+//       </section>
+//     </Layout>
+//   )
+// }
